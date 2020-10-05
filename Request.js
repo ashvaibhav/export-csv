9f7dcaa9a9e3195a55c106ca54343565184e5197
Vaibhav Agarwal <vaibhav-agarwal@enlightedinc.com>
2020-10-05 11:16:31 -0700

    Introduce Request class

---
+++ Request.js
@@ -0,0 +1,15 @@
+export default class Request {
+  constructor() {
+    this.events = {};
+  }
+
+  on(eventId, callback) {
+    this.events[eventId] = callback;
+  }
+
+  emit(eventId, data) {
+    if (this.events[eventId]) {
+      this.events[eventId](data);
+    }
+  }
+}
