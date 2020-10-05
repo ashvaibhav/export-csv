39d41b538987796764fa15892d9aabb1d04412e0
Vaibhav Agarwal <vaibhav-agarwal@enlightedinc.com>
2020-10-04 21:24:04 -0700

    Better indentation. Introduce dataStreamer

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const faker = require('faker');
-const fs = require('fs');
+const faker = require("faker");
+const fs = require("fs");
 
-const writeUsers = fs.createWriteStream('users.csv');
-writeUsers.write('id,username,avatar\n', 'utf8');
+const writeUsers = fs.createWriteStream("users.csv");
+writeUsers.write("id,username,avatar\n", "utf8");
 
 function writeTenMillionUsers(writer, encoding, callback) {
   let i = 10000000;
@@ -12,26 +12,31 @@ function writeTenMillionUsers(writer, encoding, callback) {
     do {
       i -= 1;
       id += 1;
-      const username = faker.internet.userName();
-      const avatar = faker.image.avatar();
-      const data = `${id},${username},${avatar}\n`;
+      const data = dataStreamer(id);
       if (i === 0) {
         writer.write(data, encoding, callback);
       } else {
-// see if we should continue, or wait
-// don't pass the callback, because we're not done yet.
+        // see if we should continue, or wait
+        // don't pass the callback, because we're not done yet.
         ok = writer.write(data, encoding);
       }
     } while (i > 0 && ok);
     if (i > 0) {
-// had to stop early!
-// write some more once it drains
-      writer.once('drain', write);
+      // had to stop early!
+      // write some more once it drains
+      writer.once("drain", write);
     }
   }
-write()
+  write();
 }
 
-writeTenMillionUsers(writeUsers, 'utf-8', () => {
+function dataStreamer(id) {
+  const username = faker.internet.userName();
+  const avatar = faker.image.avatar();
+  const data = `${id},${username},${avatar}\n`;
+  return data;
+}
+
+writeTenMillionUsers(writeUsers, "utf-8", () => {
   writeUsers.end();
 });
